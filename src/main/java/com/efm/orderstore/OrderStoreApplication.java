28a8b2650f421e49a86f98a8b533dc0dd5cc07c5
eduardofmarcos <eduardomarcos_@outlook.com>
2020-08-15 19:09:14 -0300

    first commit/ created project

---
+++ src/main/java/com/efm/orderstore/OrderStoreApplication.java
@@ -0,0 +1,13 @@
+package com.efm.orderstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efm/orderstore/OrderStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efm.orderstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
