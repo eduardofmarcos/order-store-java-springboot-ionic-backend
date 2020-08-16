606285862a5b168e8f4a4125eef2299d27b10b85
eduardofmarcos <eduardomarcos_@outlook.com>
2020-08-16 14:09:06 -0300

    implemented h2-database

--- src/main/java/com/efm/orderstore/domain/Category.java
+++ src/main/java/com/efm/orderstore/domain/Category.java
@@ -1,11 +1,17 @@
 package com.efm.orderstore.domain;
-
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 
