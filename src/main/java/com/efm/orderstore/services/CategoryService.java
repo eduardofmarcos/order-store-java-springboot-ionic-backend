c5a006169650956b7f75d27a938857d9a63838c0
eduardofmarcos <eduardomarcos_@outlook.com>
2020-08-18 16:20:36 -0300

    Implemented Put Category

--- src/main/java/com/efm/orderstore/services/CategoryService.java
+++ src/main/java/com/efm/orderstore/services/CategoryService.java
@@ -1,4 +1,5 @@
 package com.efm.orderstore.services;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +11,24 @@ import com.efm.orderstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	public Category findById(Integer id) {
 		Optional<Category> obj = categoryRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Object not found. Id: " + id + ", Type: " + Category.class.getName()));
-		}
-	
+				"Object not found. Id: " + id + ", Type: " + Category.class.getName()));
+	}
+
 	public Category insert(Category obj) {
 		obj.setId(null);
 		return categoryRepository.save(obj);
+	}
+
+	public Category update(Category obj) {
+		findById(obj.getId());
+		return categoryRepository.save(obj);
+
 	}
 }
