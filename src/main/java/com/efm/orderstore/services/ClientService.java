685a05e68c7348d0f33b267afcb6be981f4138a0
eduardofmarcos <eduardomarcos_@outlook.com>
2020-08-19 19:33:54 -0300

    Implemented Insert Client

--- src/main/java/com/efm/orderstore/services/ClientService.java
+++ src/main/java/com/efm/orderstore/services/ClientService.java
@@ -1,4 +1,5 @@
 package com.efm.orderstore.services;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -9,24 +10,34 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.efm.orderstore.domains.Address;
+import com.efm.orderstore.domains.City;
 import com.efm.orderstore.domains.Client;
+import com.efm.orderstore.domains.enums.ClientType;
 import com.efm.orderstore.dto.ClientDTO;
+import com.efm.orderstore.dto.ClientNewDTO;
+import com.efm.orderstore.repositories.AddressRepository;
+import com.efm.orderstore.repositories.CityRepository;
 import com.efm.orderstore.repositories.ClientRepository;
 import com.efm.orderstore.services.exceptions.DataIntegrityException;
 import com.efm.orderstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository clientRepository;
-	
+	@Autowired
+	private CityRepository cityRepository;
+	@Autowired
+	private AddressRepository addressRepository;
+
 	public Client findById(Integer id) {
 		Optional<Client> obj = clientRepository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Object not found. Id: " + id + ", Type: " + Client.class.getName()));
-		}
-	
+		return obj.orElseThrow(
+				() -> new ObjectNotFoundException("Object not found. Id: " + id + ", Type: " + Client.class.getName()));
+	}
+
 	public List<Client> findAll() {
 		return clientRepository.findAll();
 	}
@@ -46,24 +57,51 @@ public class ClientService {
 	private void updateDate(Client newObj, Client obj) {
 		newObj.setName(obj.getName());
 		newObj.setEmail(obj.getEmail());
-		
-}
+
+	}
+
+	@org.springframework.transaction.annotation.Transactional
+	public Client insert(Client obj) {
+		obj.setId(null);
+		obj = clientRepository.save(obj);
+		addressRepository.saveAll(obj.getAddresses());
+		return obj;
+	}
 
 	public void delete(Integer id) {
 		findById(id);
 		try {
-		clientRepository.deleteById(id);
-		}catch (DataIntegrityViolationException e){
+			clientRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("It is not possible to delete a category with associated products");
 		}
 	}
-	
-	public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+	public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return clientRepository.findAll(pageRequest);
 	}
-	
+
 	public Client fromDTO(ClientDTO objDTO) {
-		return new Client(objDTO.getId(),objDTO.getName(),objDTO.getEmail(),null,null);
+		return new Client(objDTO.getId(), objDTO.getName(), objDTO.getEmail(), null, null);
 	}
+
+	public Client fromDTO(ClientNewDTO objDTO) {
+		Client newClient = new Client(null, objDTO.getName(), objDTO.getEmail(), objDTO.getCpfOrCnpj(),
+				ClientType.toEnum(objDTO.getClientType()));
+		City city = new City(objDTO.getCityId(), null, null);
+		// Optional<City> city = cityRepository.findById(objDTO.getCityId());
+		Address address = new Address(null, objDTO.getStreet(), objDTO.getNumber(), objDTO.getComplement(),
+				objDTO.getDistrict(), objDTO.getCep(), newClient, city);
+		newClient.getAddresses().add(address);
+		newClient.getPhoneList().add(objDTO.getPhone1());
+		if (objDTO.getPhone2() != null) {
+			newClient.getPhoneList().add(objDTO.getPhone2());
+		}
+		if (objDTO.getPhone3() != null) {
+			newClient.getPhoneList().add(objDTO.getPhone3());
+		}
+		return newClient;
+	}
+
 }
