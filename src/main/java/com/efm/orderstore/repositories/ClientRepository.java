cb86a6540d8e04bb98151125adc32298c24998e4
eduardofmarcos <eduardomarcos_@outlook.com>
2020-08-20 12:32:38 -0300

    Implemented duplicate email verification

--- src/main/java/com/efm/orderstore/repositories/ClientRepository.java
+++ src/main/java/com/efm/orderstore/repositories/ClientRepository.java
@@ -2,10 +2,14 @@ package com.efm.orderstore.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.efm.orderstore.domains.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
+	@Transactional(readOnly=true)
+	Client findByEmail(String email);
+	
 }
--- src/main/java/com/efm/orderstore/services/validations/ClientInsertValidation.java
+++ src/main/java/com/efm/orderstore/services/validations/ClientInsertValidation.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.efm.orderstore.domains.Client;
 import com.efm.orderstore.domains.enums.ClientType;
 import com.efm.orderstore.dto.ClientNewDTO;
+import com.efm.orderstore.repositories.ClientRepository;
 import com.efm.orderstore.resources.exceptions.FieldMessage;
 import com.efm.orderstore.services.validations.utils.BR;
 
 public class ClientInsertValidation implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	@Override
 	public void initialize(ClientInsert ann) {
 	};
@@ -26,6 +34,12 @@ public class ClientInsertValidation implements ConstraintValidator<ClientInsert,
 		
 		if(objDTO.getClientType().equals(ClientType.PESSOAJURIDICA.getCode()) && !BR.isValidCNPJ(objDTO.getCpfOrCnpj())) {
 			list.add(new FieldMessage("cpfOrCnpj", "Invalid CNPJ"));
+		}
+		
+		Client clientToFind = clientRepository.findByEmail(objDTO.getEmail());
+		
+		if(clientToFind!= null) {
+			list.add(new FieldMessage("email", "Email already already exists"));
 		}
 		
 		for (FieldMessage el : list) {
